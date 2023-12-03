a659fe1b1d43d0b2316c4b1d1d43baa15e7a0d6c
subhash1599 <subhashnimmala01@gmail.com>
2023-12-03 14:53:34 +0530

    code complete

--- can_place_flowers.py
+++ can_place_flowers.py
@@ -1,4 +1,5 @@
 def can_place_flowers(flower_bed,new_flowers):
+
     count=0
     flower_bed=[0]+flower_bed+[0]
     for i in range(len(flower_bed)):
@@ -7,6 +8,7 @@ def can_place_flowers(flower_bed,new_flowers):
                 flower_bed[i]=1
                 count+=1
     return count>=new_flowers
+
 
 if __name__=="__main__":
     print(can_place_flowers([1,0,0,0,1],1))
